14455326af9cc732d64acd2c6e16fb56e30e0af7
swl632 <132548334+swl632@users.noreply.github.com>
2024-10-30 18:47:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -326,11 +326,6 @@ class Summary(Plugin):
         help_text = "聊天记录总结插件。\n"
         if not verbose:
             return help_text
-        trigger_prefix = conf().get('plugin_trigger_prefix', "$")
-        #########
-
-        help_text += "\n已记录的聊天记录如下：\n"
-
-        ##########        
-        #help_text += f"使用方法:输入\"{trigger_prefix}总结 最近消息数量\"，我会帮助你总结聊天记录。\n例如：\"{trigger_prefix}总结 100\"，我会总结最近100条消息。\n\n你也可以直接输入\"{trigger_prefix}总结前99条信息\"或\"{trigger_prefix}总结3小时内的最近10条消息\"\n我会尽可能理解你的指令。"
+        trigger_prefix = conf().get('plugin_trigger_prefix', "$")  
+        help_text += f"使用方法:输入\"{trigger_prefix}总结 最近消息数量\"，我会帮助你总结聊天记录。\n例如：\"{trigger_prefix}总结 100\"，我会总结最近100条消息。\n\n你也可以直接输入\"{trigger_prefix}总结前99条信息\"或\"{trigger_prefix}总结3小时内的最近10条消息\"\n我会尽可能理解你的指令。"
         return help_text
